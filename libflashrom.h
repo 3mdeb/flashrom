f2dc94485df771288e5d94aeb71029ad6af03652
Artur Raglis <artur.raglis@3mdeb.com>
2018-10-26 18:34:25 +0200

    libflashrom: add querying functions
    Change-Id: Ia95eb3976faa792bc215652f2e56deffa6e5cce1

--- libflashrom.h
+++ libflashrom.h
@@ -33,10 +33,62 @@ enum flashrom_log_level {
 	FLASHROM_MSG_DEBUG2	= 4,
 	FLASHROM_MSG_SPEW	= 5,
 };
+
+typedef struct flashrom_context{
+	const void *programmer_context;
+	const void *chip_context;
+	const void *log_context;
+} flashrom_context;
+
 /** @ingroup flashrom-general */
 typedef int(flashrom_log_callback)(enum flashrom_log_level, const char *format, va_list);
 void flashrom_set_log_callback(flashrom_log_callback *);
 
+/** @ingroup flashrom-query */
+typedef enum{
+  FLASHROM_TESTED_OK  = 0,
+  FLASHROM_TESTED_NT  = 1,
+  FLASHROM_TESTED_BAD = 2,
+  FLASHROM_TESTED_DEP = 3,
+  FLASHROM_TESTED_NA  = 4,
+} flashrom_test_state;
+
+typedef struct flashrom_flashchip_info{
+	const char *vendor;
+	const char *name;
+	unsigned int total_size;
+	struct flashrom_tested {
+		flashrom_test_state probe;
+		flashrom_test_state read;
+		flashrom_test_state erase;
+		flashrom_test_state write;
+	} tested;
+} flashrom_flashchip_info;
+
+typedef struct flashrom_board_info{
+  const char *vendor;
+  const char *name;
+  flashrom_test_state working;
+} flashrom_board_info;
+
+typedef struct flashrom_chipset_info{
+  const char *vendor;
+  const char *chipset;
+	uint16_t vendor_id;
+	uint16_t chipset_id;
+  flashrom_test_state status;
+} flashrom_chipset_info;
+
+const char *flashrom_version_info(void);
+//const char **flashrom_system_info(void);
+void flashrom_system_info(void);
+const char **flashrom_supported_programmers(void);
+flashrom_flashchip_info *flashrom_supported_flash_chips(void);
+flashrom_board_info *flashrom_supported_boards(void);
+flashrom_chipset_info *flashrom_supported_chipsets(void);
+int flashrom_data_free(void *const p);
+
+/** @ingroup flashrom-prog */
 struct flashrom_programmer;
 int flashrom_programmer_init(struct flashrom_programmer **, const char *prog_name, const char *prog_params);
 int flashrom_programmer_shutdown(struct flashrom_programmer *);
@@ -63,10 +115,6 @@ int flashrom_image_verify(struct flashrom_flashctx *, const void *buffer, size_t
 
 struct flashrom_layout;
 int flashrom_layout_read_from_ifd(struct flashrom_layout **, struct flashrom_flashctx *, const void *dump, size_t len);
-int flashrom_layout_read_fmap_from_rom(struct flashrom_layout **,
-		struct flashrom_flashctx *, off_t offset, size_t length);
-int flashrom_layout_read_fmap_from_buffer(struct flashrom_layout **layout,
-		struct flashrom_flashctx *, const uint8_t *buf, size_t len);
 int flashrom_layout_include_region(struct flashrom_layout *, const char *name);
 void flashrom_layout_release(struct flashrom_layout *);
 void flashrom_layout_set(struct flashrom_flashctx *, const struct flashrom_layout *);
